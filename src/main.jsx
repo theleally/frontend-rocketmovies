4bce81585811dfb8017e06bac48a818b83ede4fc
Leandro Allysson <64550528+theleally@users.noreply.github.com>
2024-09-03 17:01:55 -0300

    styled-components

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { App } from './App'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from "./styles/global"
+import theme from "./styles/theme"
+import { Details } from './pages/Details'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Details />
+    </ThemeProvider>
   </StrictMode>,
 )
